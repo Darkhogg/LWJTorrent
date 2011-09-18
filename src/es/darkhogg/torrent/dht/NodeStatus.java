33a3e757c9d5bc06888df2eb77e7c5f675e8f679
Daniel Escoz <darkhogg@gmail.com>
2011-09-18 22:59:00 +0200

    Added `NodeStatus`
    * Added `NodeStatus` enum to represent the corrent status of a node in the routing table

---
+++ src/es/darkhogg/torrent/dht/NodeStatus.java
@@ -0,0 +1,11 @@
+package es.darkhogg.torrent.dht;
+
+/**
+ * Possible status of DHT nodes, depending on its activity and replied queries.
+ * 
+ * @author Daniel Escoz
+ * @version 1.0
+ */
+public enum NodeStatus {
+	UNKNOWN, BAD, QUESTIONABLE, GOOD;
+}
